64cf8049ee0ca9eebf64eecd1edbf477f58a7030
agmt92 <155798390+agmt92@users.noreply.github.com>
2024-08-09 02:19:21 +0200

    First commit of Running the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  clear,
+  inputNumber,
+  inputOperator,
+  inputDecimal,
+  calculate,
+} from './redux';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CalculatorButtons extends React.Component {
+  handleClick = (event) => {
+    const button = event.target;
+    const buttonValue = button.innerHTML;
+
+    if (button.id === 'clear') {
+      this.props.clear();
+    } else if (['+', '-', '*', '/'].includes(buttonValue)) {
+      this.props.inputOperator(buttonValue);
+    } else if (button.id === 'decimal') {
+      this.props.inputDecimal();
+    } else if (button.id === 'equals') {
+      this.props.calculate();
+    } else {
+      this.props.inputNumber(buttonValue);
+    }
+  };
+
+  render() {
+    return (
+      <div className="calculator-buttons">
+        <button id="clear" className="ac-button" onClick={this.handleClick}>AC</button>
+        <button id="divide" className="button division side" onClick={this.handleClick}>/</button>
+        <button id="seven" className="button" onClick={this.handleClick}>7</button>
+        <button id="eight" className="button" onClick={this.handleClick}>8</button>
+        <button id="nine" className="button" onClick={this.handleClick}>9</button>
+        <button id="multiply" className="button side" onClick={this.handleClick}>*</button>
+        <button id="four" className="button" onClick={this.handleClick}>4</button>
+        <button id="five" className="button" onClick={this.handleClick}>5</button>
+        <button id="six" className="button" onClick={this.handleClick}>6</button>
+        <button id="subtract" className="button side" onClick={this.handleClick}>-</button>
+        <button id="one" className="button" onClick={this.handleClick}>1</button>
+        <button id="two" className="button" onClick={this.handleClick}>2</button>
+        <button id="three" className="button" onClick={this.handleClick}>3</button>
+        <button id="add" className="add-button side" onClick={this.handleClick}>+</button>
+        <button id="zero" className="zero-button" onClick={this.handleClick}>0</button>
+        <button id="decimal" className="button" onClick={this.handleClick}>.</button>
+        <button id="equals" className="equal-button side" onClick={this.handleClick}>=</button>
+      </div>
+          );
+  }
 }
 
-export default App;
+const mapDispatchToProps = {
+  clear,
+  inputNumber,
+  inputOperator,
+  inputDecimal,
+  calculate,
+};
+
+const ConnectedCalculatorButtons = connect(null, mapDispatchToProps)(CalculatorButtons);
+
+class CalculatorDisplay extends React.Component {
+  render() {
+    return (
+      <div className="calculator-display">
+        <div id="display-formula">{this.props.formula}</div>
+        <div id="display-answer">{this.props.answer}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    formula: state.calculator.formula,
+    answer: state.calculator.answer,
+  };
+};
+
+const ConnectedCalculatorDisplay = connect(mapStateToProps)(CalculatorDisplay);
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+        <ConnectedCalculatorDisplay />
+        <ConnectedCalculatorButtons />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+const ConnectedApp = connect(mapStateToProps)(App);
+
+export default ConnectedApp;
---
+++ src/redux.js
@@ -0,0 +1,190 @@
+import { createStore, combineReducers } from 'redux';
+
+// Actions
+const CLEAR = 'CLEAR';
+const INPUT_NUMBER = 'INPUT_NUMBER';
+const INPUT_OPERATOR = 'INPUT_OPERATOR';
+const INPUT_DECIMAL = 'INPUT_DECIMAL';
+const CALCULATE = 'CALCULATE';
+
+// To load on codepen.io remove the export keyword and keep the declared consts and read the bottom of the file
+
+export const clear = () => ({
+  type: CLEAR,
+});
+
+export const inputNumber = (number) => ({
+  type: INPUT_NUMBER,
+  payload: number,
+});
+
+export const inputOperator = (operator) => ({
+  type: INPUT_OPERATOR,
+  payload: operator,
+});
+
+export const inputDecimal = () => ({
+  type: INPUT_DECIMAL,
+});
+
+export const calculate = () => ({
+  type: CALCULATE,
+});
+
+
+
+// Reducer
+const initialState = {
+  formula: '',
+  mutableFormula: '',
+  prevButton: '',
+  answer: '',
+};
+
+const calculatorReducer = (state = initialState, action) => {
+  const operators = ['+', '-', '*', '/'];
+  switch (action.type) {
+    case CLEAR:
+      return {
+        ...state,
+        formula: '',
+        answer: '',
+        mutableFormula: '',
+        prevButton: '',
+      };
+    case INPUT_NUMBER:
+      const firstNumber = state.formula[0];
+      if (firstNumber === '0' && state.formula.length === 1) {
+        return {
+          ...state,
+          formula: action.payload,
+          answer: action.payload,
+          mutableFormula: action.payload,
+          prevButton: action.payload,
+        };
+      } else if (operators.includes(state.formula.length - 1) ) {
+        return {          
+          ...state,
+          formula: action.payload,
+          answer: action.payload,
+          mutableFormula: action.payload,
+          prevButton: action.payload,
+        };
+      } else{
+        return {
+          ...state,
+          formula: state.formula + action.payload,
+          answer: state.mutableFormula + action.payload,
+          mutableFormula: state.mutableFormula + action.payload,
+          prevButton: action.payload,
+        }
+      };
+    case INPUT_OPERATOR:
+      if (operators.includes(state.formula[state.formula.length - 1])) {
+        return {
+          ...state,
+          formula: state.formula.slice(0, -1) + action.payload,
+          answer: action.payload,
+          mutableFormula: action.payload,
+          prevButton: action.payload,
+        };
+      } else if (state.formula === '' && action.payload === '-') {
+        return {
+          ...state,
+          formula: action.payload,
+          answer: action.payload,
+          mutableFormula: action.payload,
+          prevButton: action.payload,
+        };
+      } else if (state.formula !== '' && !operators.includes(state.formula[state.formula.length - 1])) {
+        return {
+          ...state,
+          formula: state.formula + action.payload,
+          answer: action.payload,
+          mutableFormula: '',
+          prevButton: action.payload,
+        };
+      }
+      return state;
+    case INPUT_DECIMAL:
+      if (state.formula === '') {
+        return {
+          ...state,
+          formula: '0.',
+          answer: '0.',
+          mutableFormula: '0.',
+          prevButton: action.payload,
+        };
+      } else if (!state.formula.includes('.')) {
+        return {
+          ...state,
+          formula: state.formula + '.',
+          answer: state.answer + '.',
+          mutableFormula: state.mutableFormula + '.',
+          prevButton: action.payload,
+        };
+      }
+      return state;
+    case CALCULATE:
+      try {
+        // eslint-disable-next-line no-eval
+        const answer = eval(state.formula);
+        if (state.answer === state.formula) {
+          return {
+            ...state,
+            // eslint-disable-next-line no-eval
+            formula: eval(state.answer + '*' + state.answer),
+            prevButton: action.payload,
+            // eslint-disable-next-line no-eval
+            mutableFormula: eval(state.answer + '*' + state.answer),
+            // eslint-disable-next-line no-eval
+            answer: eval(state.answer + '*' + state.answer),
+          };
+        } else if (state.answer !== state.formula && state.prevButton === '=' ) {
+          return {
+            ...state,
+            formula: state.answer,
+            // eslint-disable-next-line no-eval
+            mutableFormula: eval(state.answer + '*' + state.answer),
+            prevButton: action.payload,
+          };
+          } else {
+          return {
+            ...state,
+            answer: answer.toString(),
+            mutableFormula: '',
+          };
+        }
+      } catch (error) {
+        return state;
+      }
+    default:
+      return state;
+  }
+};
+
+// Store
+const rootReducer = combineReducers({
+  calculator: calculatorReducer,
+});
+
+const store = createStore(rootReducer);
+
+export { store };
+
+//codepen.io dependencies
+
+// const { useState, useEffect } = React;
+// const { Provider, connect } = ReactRedux;
+// const { createStore, combineReducers } = Redux;
+
+// add the code from App.js
+// add the code from Index.js
+
+// Activate Babel preprocessor in Codepen settings
+// Add the following external scripts in the JS settings:
+// https://unpkg.com/react@17/umd/react.development.js
+// https://unpkg.com/react-dom@17/umd/react-dom.development.js
+// https://cdnjs.cloudflare.com/ajax/libs/redux/5.0.1/redux.legacy-esm.js
+// https://cdnjs.cloudflare.com/ajax/libs/redux/4.1.2/redux.min.js
+// https://cdnjs.cloudflare.com/ajax/libs/react-redux/7.2.6/react-redux.min.js
